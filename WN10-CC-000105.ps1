9b0e0e3b88f9e29ccbb225076b9a7d442befd409
Orlando G. <greetingsog@gmail.com>
2026-02-01 16:59:29 -0500

    Update WN10-CC-000105.ps1

--- WN10-CC-000105.ps1
+++ WN10-CC-000105.ps1
@@ -18,10 +18,10 @@
     STIG-ID         : WN10-CC-000105
 
 .TESTED ON
-    Date(s) Tested  : 
-    Tested By       : 
-    Systems Tested  : 
-    PowerShell Ver. : 
+    Date(s) Tested  : 2025-11-07
+    Tested By       : OG
+    Systems Tested  : Windows 10 Pro
+    PowerShell Ver. : 5.1
 
 .USAGE
     Put any usage instructions here.
