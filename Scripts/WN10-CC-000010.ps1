868964cb38b2547a85e187b49fa51fe7fd9d1de4
Orlando G. <greetingsog@gmail.com>
2026-02-01 17:21:40 -0500

    Update and rename WN10-CC-000010.ps1 to Scripts/WN10-CC-000010.ps1

--- Scripts/WN10-CC-000010.ps1
+++ Scripts/WN10-CC-000010.ps1
@@ -18,10 +18,10 @@
     STIG-ID         : WN10-CC-000010
 
 .TESTED ON
-    Date(s) Tested  : 
-    Tested By       : 
-    Systems Tested  : 
-    PowerShell Ver. : 
+    Date(s) Tested  : 2025-11-12
+    Tested By       : OG
+    Systems Tested  : Windows 10 Pro
+    PowerShell Ver. : 5.1
 
 .USAGE
     Put any usage instructions here.
